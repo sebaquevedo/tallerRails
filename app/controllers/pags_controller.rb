e7731888de7e5a466ed1a908b48ad8a0dcc5538f
sebastian quevedo <sebaqd91@gmail.com>
2017-04-06 18:36:48 -0300

    added backstretch

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,16 @@
 class PagsController < ApplicationController
-  def pag1
-  end
+	before_action :get_data
+	def pag1
+		 render :layout => "landing"
+	end
 
-  def pag2
-  end
+	def pag2
+	end
+
+	def pag3
+	end
+
+	def get_data
+		@users = ["Alumno1","Alumno2","Alumno3"]
+	end
 end
